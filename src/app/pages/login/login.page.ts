4c5d3ebec08f8612bb0ca0bb101ce5b99d459796
carlitosAlvz <carlosalvzflores@gmail.com>
2022-09-12 17:24:34 -0500

    instalando aplicacion en el celular

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  bandBotones: Boolean = true;
+  bandLogin: Boolean = false;
+  constructor() { 
   }
 
+  ngOnInit() {
+  
+  }
+  cambiodeInterfaz(){
+    console.log("si entro");
+    this.bandBotones = false;
+    this.bandLogin = true;
+  }
 }
